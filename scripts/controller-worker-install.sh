c388d13d898d4d25ae97e8f25b0ca7df33212a80
Wriju Ghosh <wriju@hotmail.com>
2021-02-27 14:28:46 +0530

    Hello

--- scripts/controller-worker-install.sh
+++ scripts/controller-worker-install.sh
@@ -3,10 +3,9 @@
 #=================================================================
 # Run following commands to install docker, kubelet, kubeadm and kubectl:
 
-# -----------DOCKER--------------------
+# echo "-----------------DOCKER--------------------"
 echo "------------------------ Install curl, https etc  ----------------------------"
 # (Install Docker CE)
-## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
 sudo apt-get update && sudo apt-get install -y \
   apt-transport-https ca-certificates curl software-properties-common gnupg2
@@ -25,26 +24,6 @@ sudo apt-get update && sudo apt-get install -y \
   containerd.io=1.2.13-2 \
   docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) \
   docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
-
-## Create /etc/docker
-sudo mkdir /etc/docker
-
-# Set up the Docker daemon
-cat <<EOF | sudo tee /etc/docker/daemon.json
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
-
-sudo mkdir -p /etc/systemd/system/docker.service.d
-
-sudo usermod -aG docker $USER
-newgrp docker
 
 # -------------------
 echo "------------------------ Install kubelet, kubeadm, kubectl ----------------------------"
@@ -66,32 +45,22 @@ echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
 echo "------------------------ Restarting Docker ----------------------------"
-# Restart Docker
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-sudo systemctl enable docker
-# ****************************************************************
-#-------------------Run the below only in Controll plane 
-#https://github.com/pranabpaul/k8scerts/blob/main/configure
-#cert=b41a209250e1c96bad535d10c5e6efd0e2acce060cf03a33bf93ada25e1f5e8a
-#cert=$(kubeadm certs certificate-key)
-# sudo systemctl enable docker.service
-# sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --upload-certs --certificate-key $(kubeadm certs certificate-key)
+## Create /etc/docker
+sudo mkdir -p /etc/docker
 
-# #-----------------
-# # Sample Output
-# # ----------------
-# # kubeadm join 10.230.0.10:6443 --token ku8zyq.260duqkosz4uw5bk \
-# #     --discovery-token-ca-cert-hash sha256:5ff0d2965d38ff21f211e62c16d39ce6a5fd820a7713a37cd158fd954312fa61
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
 
-# # To find later-------------
-# # kubeadm token create --print-join-command
+sudo mkdir -p /etc/systemd/system/docker.service.d
 
-# mkdir -p $HOME/.kube
-# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-# sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# # Install Claico
-# kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
-
-#Run the "Sample Output" to workers nodes kubeadm join
+sudo usermod -aG docker $USER
+newgrp docker
