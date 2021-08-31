5f659ec01bed6ae375dcb509d061a9878946f20e
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2021-08-31 16:46:49 +0530

    Client VM

---
+++ script-aug21/05-kubectl-client.sh
@@ -0,0 +1,28 @@
+echo '--------------------Client VM---------------'
+
+echo '-------------------- 1 IP table --------------------'
+
+lsmod | grep br_netfilter
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+
+echo '-------------------- 2 Install kubectl'
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update && sudo apt-get install -y kubectl
+sudo apt-mark hold kubectl
